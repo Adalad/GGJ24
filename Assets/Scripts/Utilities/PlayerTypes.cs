5671ce487f35c28c1cbe7e5f9aa1bafab43b49ed
Adalad <8684790+Adalad@users.noreply.github.com>
2024-01-27 21:23:44 +0100

    Fight but remove rsources

--- Assets/Scripts/Utilities/PlayerTypes.cs
+++ Assets/Scripts/Utilities/PlayerTypes.cs
@@ -4,4 +4,5 @@ public class PlayerTypes
     public static int[] playerAsignedCharacter = new int[4];
     public static int[] TeamAChoices = new int[6];
     public static int[] TeamBChoices = new int[6];
+    public static int[] TeamScores = new int[2];
 }
